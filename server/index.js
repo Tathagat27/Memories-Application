144253eea6a2c50225706ac00c388b414512f437
Tathagat27 <88197781+Tathagat27@users.noreply.github.com>
2023-01-06 06:52:32 +0530

    Privacy changes

--- server/index.js
+++ server/index.js
@@ -14,7 +14,7 @@ app.use(cors());
 
 app.use('/posts', postRoutes)
 
-const CONNECTION_URL = 'mongodb+srv://tathagat:tathagat123@cluster0.qr6go8j.mongodb.net/?retryWrites=true&w=majority';
+const CONNECTION_URL = 'mongodb+srv://[Username]:[Password]@cluster0.qr6go8j.mongodb.net/?retryWrites=true&w=majority';
 const PORT = process.env.PORT || 5000;
 
 mongoose.connect(CONNECTION_URL, {useNewUrlParser: true, useUnifiedTopology: true})
